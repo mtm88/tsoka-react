482982cc9776995239da6c323964d162c7003045
Michal <michal@mysolomon.co.uk>
2018-04-28 10:08:30 +0200

    working home screen

--- src/Screens/main.js
+++ src/Screens/main.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, StyleSheet, Platform, Image, ImageBackground } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Platform, ImageBackground, Image as RNImage } from 'react-native';
 import { Icon, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
+import { Image } from "react-native-expo-image-cache";
 
 import { list } from './../reducer';
 
 class AccomodationList extends Component {
   static navigationOptions = ({ navigation }) => ({
     drawerLabel: 'Home',
-    headerStyle: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      zIndex: 100,
-      backgroundColor: 'transparent',
-    },
-    headerLeft: (
-      <Icon
-        name='menu'
-        color='#fff'
-        size={35}
-        containerStyle={{ paddingLeft: 20 }}
-        onPress={() => navigation.navigate('DrawerToggle')} />
-    ),
+    header: 'none',
   })
 
   componentDidMount() {
@@ -31,22 +17,24 @@ class AccomodationList extends Component {
   }
 
   renderItem = ({ item }) => {
-    const uri = `${serverUrl}/images/places/${item.image}`;
+    const preview = { uri: `${serverUrl}/images/places/${item.image}-thumb.png` };
+    const uri = `${serverUrl}/images/places/${item.image}.png`;
     return (
       <View style={styles.item}>
         <View style={{ overflow: 'hidden', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}>
           <Image
             style={{ height: 120, width: null }}
-            source={{ uri }}
+            {...{
+              preview,
+              uri,
+            }}
           />
         </View>
-        <View style={{ flex: 1, paddingLeft: 15, paddingBottom: 15 }}>
-          <Text style={{
-            ...fontFamily, color: '#4c0f00', fontSize: 18, paddingTop: 15, fontWeight: 'bold'
-          }}>{item.name}</Text>
-          <Text style={{ color: '#ffb41d', fontSize: 15, paddingTop: 15 }}>{`${Math.floor(Math.random() * (40 - 2 + 1)) + 2} Things To Do`}</Text>
+        <View style={{ flex: 1, paddingLeft: 15, paddingBottom: 5 }}>
+          <Text style={{ ...fontFamily, color: '#4c0f00', fontSize: 20, paddingTop: 14, fontWeight: 'bold' }}>{item.name}</Text>
+          <Text style={{ ...fontFamily, color: '#ffb41d', fontSize: 15, paddingTop: 16 }}>{`${Math.floor(Math.random() * (40 - 2 + 1)) + 2} Things To Do`}</Text>
 
-          <View style={{ flexDirection: 'row', paddingTop: 15 }}>
+          <View style={{ flexDirection: 'row', paddingTop: 5 }}>
             <Text style={{ ...fontFamily, color: '#597580', fontSize: 21 }}>${item.cost_per_night ? parseInt(item.cost_per_night, 10).toFixed() : Math.floor(Math.random() * (200 - 30 + 1)) + 30}</Text>
             <Text style={{ ...fontFamily, color: '#597580', fontSize: 17, paddingTop: 3 }}> per person</Text>
           </View>
@@ -59,11 +47,12 @@ class AccomodationList extends Component {
     return (
       <View style={styles.container}>
         <ImageBackground
-          style={{ flex: 1, width: null, height: null }}
+          resizeMode='stretch'
+          style={{ flex: 1, width: null, height: null, minHeight: 30, alignItems: 'center' }}
           source={require('./../../static/images/home.jpg')}>
 
-          <View pointerEvents='none' style={{
-            backgroundColor: 'rgba(83,60,49,0.5)',
+          <View style={{
+            backgroundColor: 'rgba(126,67,41,0.5)',
             position: 'absolute',
             left: 0,
             top: 0,
@@ -71,17 +60,34 @@ class AccomodationList extends Component {
             right: 0,
           }}></View>
 
-          <View style={{ flex: 1 }}>
-            <Image
-              style={{ width: 128, height: 64, alignSelf: 'center' }}
+          <View style={{ flex: 1, position: 'absolute', top: 8, left: 8, backgroundColor: 'transparent' }}>
+            <Icon
+              name='menu'
+              color='white'
+              size={40}
+              onPress={() => this.props.navigation.navigate('DrawerToggle')} />
+          </View>
+
+          <View style={{ flex: 5, justifyContent: 'flex-end', paddingBottom: 15 }}>
+            <RNImage
+              style={{ width: 128, height: 64 }}
               source={require('./../../static/images/logo.png')}
             />
           </View>
 
           <Input
-            containerStyle={{ flex: 1, alignSelf: 'center' }}
-            inputContainerStyle={{ backgroundColor: 'rgba(244,180,76,1)', height: 45, borderRadius: 8, alignSelf: 'center' }}
-            inputStyle={{ color: '#fff', fontSize: 20, fontWeight: 'bold' }}
+            containerStyle={{ flex: 3, alignSelf: 'center' }}
+            inputContainerStyle={{
+              backgroundColor: 'rgba(244,180,76,0.6)',
+              height: 45,
+              width: 280,
+              borderRadius: 8,
+              borderBottomWidth: 0,
+              alignSelf: 'center'
+            }}
+            inputStyle={{ fontSize: 20, fontWeight: 'bold' }}
+            autoCapitalize={'none'}
+            autoCorrect={false}
             placeholder='Where to?'
             placeholderTextColor="#fff"
             rightIcon={
@@ -103,7 +109,7 @@ class AccomodationList extends Component {
             renderItem={this.renderItem}
           />
         </View>
-      </View >
+      </View>
     );
   }
 }
@@ -122,7 +128,7 @@ const styles = StyleSheet.create({
   },
   flatList: {
     backgroundColor: '#f4b44c',
-    flex: 3,
+    flex: 2,
   },
   item: {
     flex: 1,
--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 global.env = 'development';
-global.serverUrl = 'http://localhost/partners';
+// global.serverUrl = 'http://localhost/partners';
 // global.serverUrl = 'http://192.168.0.6/partners';
+global.serverUrl = 'http://192.168.55.103/partners';
